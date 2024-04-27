e6f780e70e56af79b6a8fe167f3546c584a35de5
artyom <agasparyan0000@gmail.com>
2024-04-27 15:26:16 +0400

    small font fixes, animation added on 2nd section & svg fixed

--- js/index.js
+++ js/index.js
@@ -19,4 +19,38 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   ScrollReveal().reveal('*[data-scroll-reveal]');
+
+  var controller = new ScrollMagic.Controller();
+  var targetEl = document.querySelector('#target');
+  var videoInner = document.querySelector('.video__inner')
+
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 600, triggerHook: 1})
+
+  // .setPin("#target")
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller)
+  .on("update", function (e) {
+    // console.log('🚀 ~ e.target.controller().info("scrollDirection"):', e.target.controller().info("scrollDirection"))
+  })
+  .on("enter leave", function (e) {
+    console.log(e.type == "enter" ? "inside" : "outside");
+    if(e.type == "enter") {
+      targetEl.style.maxWidth = "initial"
+      console.log("🚀 ~ targetEl.style.maxWidth:", targetEl.style.maxWidth)
+    }
+  })
+  .on("start end", function (e) {
+    console.log(e.type == "start" ? "top" : "bottom");
+  })
+  .on("progress", function (e) {
+    let progressToFour = 4-(e.progress*4).toFixed(3)
+    let progressToEighty = 80 - (e.progress*80).toFixed(1)
+    
+    targetEl.style.padding = `0 ${progressToFour}%`
+    videoInner.style.borderRadius = `${progressToEighty}px`
+   
+
+    // if(progress > 0.000)
+    // if(e.progress.)
+  });;
 })
