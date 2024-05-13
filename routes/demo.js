61a06eb0846a9a7c069818c1c2b53f036e45cd31
artyom <agasparyan0000@gmail.com>
2024-05-13 13:36:34 +0400

    telegram notifications & emails

--- routes/demo.js
+++ routes/demo.js
@@ -1,23 +1,23 @@
 let router = require('express').Router()
 
 router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+    res.render('dashboard/dashboard')
 })
 
 router.get('/campaigns', (req, res) => {
-    res.render('campaigns')
+    res.render('dashboard/campaigns')
 })
 
 router.get('/subscription', (req, res) => {
-    res.render('subscription')
+    res.render('dashboard/subscription')
 })
 
 router.get('/support', (req, res) => {
-    res.render('support')
+    res.render('dashboard/support')
 })
 
 router.get('/earn-with-us', (req, res) => {
-    res.render('earn-with-us')
+    res.render('dashboard/earn-with-us')
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 let router = require('express').Router()
-const TelegramBot = require('node-telegram-bot-api');
+let nodemailer = require('nodemailer')
+const TelegramBot = require('node-telegram-bot-api')
+const ejs = require('ejs');
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.timeweb.ru",
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: "admin@whatsbulking.com",
+      pass: "0bzr0j3af5",
+    },
+})
+
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
 
@@ -8,7 +21,7 @@ router.get('/', (req, res) => {
 })
 
 router.post('/send', (req, res) => {
-    console.log("🚀 ~ router.post ~ req.body:", req.body)
+
     bot.sendMessage(process.env.CHAT_ID, `📌 New form request!
 
 Name: ${req.body.name}
@@ -18,6 +31,26 @@ Meta: ${req.body.metaInfo}
 Comment: ${req.body.comment}`);
 
     res.sendStatus(200)
+
+    ejs.renderFile(__dirname + '/templates/email-template.ejs', { name: req.body.name, link: 'examplelink' }, (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+            const info = transporter.sendMail({
+                from: 'admin@whatsbulking.com', // sender address
+                to: req.body.email, // list of receivers
+                subject: `WhatsBulking | Meeting for ${req.body.name}`, // Subject line
+                html: data
+            }).then((info) => {
+                  console.log(info)
+            }).catch(e => {
+                bot.sendMessage(process.env.CHAT_ID, `Error Occured Sending Message to ${req.body.email}: ${e}`);
+            })
+        }
+    })
+
+            
+        
 })
 
 module.exports = router
